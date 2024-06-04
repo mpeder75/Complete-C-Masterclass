76a92d3571b6713f25dc40ec6fd486cbdea21d58
Michael <mpeder75@gmail.com>
2024-06-04 18:32:00 +0200

    assad Nullables

---
+++ Section 2.9 - Static methods/Calc/Calculator.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Section_2._9___Static_methods.Calc
+{
+    internal class Calculator
+    { 
+        internal static int Add (int a, int b)
+        {
+            return a + b;
+        }
+    }
+}
---
+++ Section 2.9 - Static methods/Program.cs
@@ -0,0 +1,7 @@
+﻿using Section_2._9___Static_methods.Calc;
+
+// 1. Deklerere en variabel af datatypen int, kaldt result
+// 2. Som assignes (med assigment operatoren)
+// 3. Til metodekaldet Add med argumenterne 2, 2 på klassen Calculator 
+
+int result = Calculator.Add(2, 2);
